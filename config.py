e1f04aca40430f59bb54a968aa016758903b3e87
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-02-13 07:34:24 +0300

    add character template

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+class Config:
+    MARVEL_API_BASE_URL='https://gateway.marvel.com:443/v1/public/{}?apikey={}'
+    MARVEL_API_KEY_PUBLIC = os.environ.get('MARVEL_API_KEY_PUBLIC')
+    MARVEL_API_KEY_PRIVATE = os.environ.get('MARVEL_API_KEY_PRIVATE')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    UPLOADED_PHOTOS_DEST='app/static/images/avatars'
+
+    # email configurations
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    SUBJECT_PREFIX='Marvel-Tribune'
+    SENDER_EMAIL='saber.dangermouse@gmail.com'
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URL=os.environ.get('DATABASE_URL')
+    pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://saberdanger:cartoonroyalty@localhost/marvel_app_test'
+    
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://saberdanger:cartoonroyalty@localhost/marvel_app'
+    DEBUG =True
+
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
+
